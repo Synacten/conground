549cde27b0bab340a038b14d87be101acee8fe6a
Andrey <blackhorns9999@gmail.com>
2018-12-27 15:30:38 +0200

    add template engine, route

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 const Koa = require('koa');
-
-const app = new Koa();
 const logger = require('koa-pino-logger');
 const helmet = require('koa-helmet');
 const cors = require('@koa/cors');
+const path = require('path');
+const views = require('koa-views');
+const Router = require('koa-router');
+
+const router = new Router();
+const app = new Koa();
 
 app.use(logger({
   prettyPrint: true,
-  colorize: true,
 }))
   .use(helmet())
   .use(cors());
 
-app.use(async (ctx) => {
-  ctx.body = 'hi';
+app.use(views(path.join(__dirname, '/views'), {
+  extension: 'hbs',
+  map: { hbs: 'handlebars' },
+  options: {
+    partials: {
+      header: 'partials/header',
+      footer: 'partials/footer',
+    },
+  },
+}));
+
+router.get('/', async (ctx) => {
+  await ctx.render('index', {
+    title: 'Main page',
+  });
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 const port = process.env.port || 3300;
 app.listen(port);
